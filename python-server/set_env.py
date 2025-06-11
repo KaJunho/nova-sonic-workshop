3e8d9dd502946ec2f145eaff66100a04d31f37b0
Workshop Participant <participant@example.com>
2025-06-11 10:32:40 +0000

    Fixed bugs

---
+++ python-server/set_env.py
@@ -0,0 +1,10 @@
+import boto3
+
+session = boto3.Session(region_name="us-east-1")
+
+credentials = session.get_credentials().get_frozen_credentials()
+ak = credentials.access_key
+sk = credentials.secret_key
+print(f"export AWS_ACCESS_KEY_ID={ak}")
+print(f"export AWS_SECRET_ACCESS_KEY={sk}")
+print(f"export AWS_DEFAULT_REGION=us-east-1")
